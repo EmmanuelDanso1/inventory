d78277ac85e3b44441ad6e11a1f54da5451ed3ff
EmmanuelDanso1 <kwekudanso21@gmail.com>
2026-02-26 11:51:11 +0000

    form templates for the various input fields

---
+++ invent_app/forms/__init__.py
@@ -0,0 +1,4 @@
+from app.forms.item_forms import ItemForm
+from app.forms.category_forms import CategoryForm
+from app.forms.supplier_forms import SupplierForm
+from app.forms.transaction_forms import StockInForm, StockOutForm
