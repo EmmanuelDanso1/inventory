96fb3d99d330f376fdf5e4b177f2769b9080aebb
EmmanuelDanso1 <kwekudanso21@gmail.com>
2026-03-11 17:00:43 +0000

    fix seeding with python script

--- build.sh
+++ build.sh
@@ -2,12 +2,4 @@
 set -o errexit
 pip install -r requirements.txt
 flask --app invent_app:create_app db stamp head
-flask --app invent_app:create_app shell <<EOF
-from invent_app import db
-from invent_app.models.normalized.transaction_type import TransactionType
-for name in ['STOCK_IN', 'STOCK_OUT', 'ADJUSTMENT']:
-    if not db.session.query(TransactionType).filter_by(type_name=name).first():
-        db.session.add(TransactionType(type_name=name))
-db.session.commit()
-print("Transaction types seeded")
-EOF
+python seed_transaction_types.py
