78bab839d7842db05d53eee624440410c4c77bd1
EmmanuelDanso1 <kwekudanso21@gmail.com>
2026-03-11 13:13:51 +0000

    new update

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 import os
-from invent_app import create_app
+from invent_app import create_app, db
+from flask_migrate import upgrade
 
 app = create_app()
+
+with app.app_context():
+    upgrade()  # runs flask db upgrade on every startup
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
