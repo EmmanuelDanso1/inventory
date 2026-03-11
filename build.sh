84ba7e215a6056561597cd87e23a794f1837fe9e
EmmanuelDanso1 <kwekudanso21@gmail.com>
2026-03-11 17:07:50 +0000

    upgrade first tgen seed

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 pip install -r requirements.txt
-flask --app invent_app:create_app db stamp head
+flask --app invent_app:create_app db upgrade
 python seed_transaction_types.py
